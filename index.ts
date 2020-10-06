9b65e635b1e7618d09501eb799d4423de50c3564
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 16:38:37 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-commands.raw('verify', (message) => if(message.author.id == '258265415770177536'){message.reply('What an echo!');});
+commands.raw('test', (message) => if(message.author.id == '258265415770177536'){message.reply('Verified! [1]');}else{message.reply('Verified! [2]'););
