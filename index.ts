4783846817314d66bf5a6c972ffc1dce3f5406fb
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 16:43:47 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
 commands.raw('test', (message) => if(message.author.id == '258265415770177536'){message.reply('Verified! [1]');}else{message.reply('Verified! [2]'););
+discord.on("MESSAGE_CREATE", async(message) => {
+  if(message.channelId == '760887796210335825'){
+    await sleep(3000);
+    await message.deleteReaction('🍩', '155149108183695360');
+    await message.addReaction('🥯');
+  }
+})
