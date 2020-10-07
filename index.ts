3cd06ffa41fea9f27145c219b3a993dee3349dbc
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 18:53:46 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-discord.on("MESSAGE_CREATE", async(message) => {
-  if(message.channelId == '760887796210335825'){
-    await message.addReaction('🥯');
-    await sleep(2000);
-    await message.deleteReaction('🍩', '155149108183695360');
-  }
-});
+
