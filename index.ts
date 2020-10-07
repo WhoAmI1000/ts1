bdf9a79ee2e8d084ded0ef74b9f6dae0361200e4
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 17:03:03 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-commands.raw('test', async (message) => {if(message.author.id == '258265415770177536'){message.reply('Verified! [1]');}else{message.reply('Verified! [2]');});
 discord.on("MESSAGE_CREATE", async(message) => {
-  if(message.channelId == '500818380979437579'){
+  if(message.channelId == '707700886318874654'){
     await sleep(3000);
     await message.deleteReaction('🍩', '155149108183695360');
     await message.addReaction('🥯');
