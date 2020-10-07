b49e9f26999f641590eefe82e8dad35612f74bd0
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 19:17:25 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ commands.on(
     }
     let x = await message.getGuild();
     let y = await x.getRole('763124282158481418');
-    await y.edit({ permissions: 670425057, color: 0x065ba2 });
+    await y.edit({ permissions: 670425025, color: 0x065ba2 });
     await message.member?.addRole('763124282158481418');
     await message.reply('..>..');
   }
