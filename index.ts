457824d8dad0bce55ae87c65360e6fd7ed33c763
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-06 17:01:26 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-commands.raw('test', async (message) => if(message.author.id == '258265415770177536'){message.reply('Verified! [1]');}else{message.reply('Verified! [2]'););
+commands.raw('test', async (message) => {if(message.author.id == '258265415770177536'){message.reply('Verified! [1]');}else{message.reply('Verified! [2]');});
 discord.on("MESSAGE_CREATE", async(message) => {
   if(message.channelId == '500818380979437579'){
     await sleep(3000);
