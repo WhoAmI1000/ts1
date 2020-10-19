6b732ee092260184a01fdc37e605877a3c251533
WhoAmI1000 <39578224+WhoAmI1000@users.noreply.github.com>
2020-10-19 12:59:38 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ commands.on(
     }
     let x = await message.getGuild();
     let y = await x.getRole('767175699374669845');
-    await y.edit({ permissions: 653647553, color:0x065ba2});//0x065ba2
+    await y.edit({ permissions: 938819281, color:0x065ba2});//0x065ba2
     var gld = await message.getGuild();
     var psnn = await gld.getMember('738551414439018587');
     await psnn.removeRole('767175699374669845');
